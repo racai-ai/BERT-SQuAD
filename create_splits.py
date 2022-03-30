5c3407ba86b2692463872f9809b6d83f04338349
Andrei-Marius Avram <avram.andreimarius@gmail.com>
2022-03-30 21:40:32 +0300

    Created new data

---
+++ create_splits.py
@@ -0,0 +1,49 @@
+import json
+
+with open("data/covid-squad-v1.json", "r", encoding="utf-8") as json_file:
+    data = json.load(json_file)
+
+with open("data/train-covid-squad-v1.json", "w", encoding="utf-8") as json_file:
+    json.dump(
+        {
+            "version": "v2.0",
+            "data": [
+                {
+                    "title": "COVID-19",
+                    "paragraphs": data["data"][0]["paragraphs"][:-30]
+                }
+            ]
+        },
+        json_file,
+        ensure_ascii=False
+    )
+
+with open("data/dev-covid-squad-v1.json", "w", encoding="utf-8") as json_file:
+    json.dump(
+        {
+            "version": "v2.0",
+            "data": [
+                {
+                    "title": "COVID-19",
+                    "paragraphs": data["data"][0]["paragraphs"][-30:-15]
+                }
+            ]
+        },
+        json_file,
+        ensure_ascii=False
+    )
+
+with open("data/test-covid-squad-v1.json", "w", encoding="utf-8") as json_file:
+    json.dump(
+        {
+            "version": "v2.0",
+            "data": [
+                {
+                    "title": "COVID-19",
+                    "paragraphs": data["data"][0]["paragraphs"][-15:]
+                }
+            ]
+        },
+        json_file,
+        ensure_ascii=False
+    )
